c93cb4c15479edbb3d4c43208bc69530e3d61cda
Braden <bsw0014@uah.edu>
2021-11-05 18:58:05 -0500

    Encryptor Class

---
+++ encryptor/encryptor.py
@@ -0,0 +1,28 @@
+
+import random
+
+class DHKE(object):
+    
+    def __init__(self, public_key1, public_key2, private_key):
+
+
+
+
+
+    def generate_partial_key(self):
+
+
+    
+
+
+    def generate_full_key(self):
+
+
+
+
+    def encrypt_message(self,message):
+
+
+
+
+    def decrypt_message(self, encrypted_message)
